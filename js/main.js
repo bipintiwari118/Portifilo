a9422a5a39d1fe34bc7bc11cb2f2be0c565065a1
Bipin Tiwari <bipintiwari118@gmail.com>
2023-09-19 19:33:53 +0545

    update

--- js/main.js
+++ js/main.js
@@ -1,104 +1,152 @@
 (function ($) {
-	"use strict";
-	var nav = $('nav');
-	var navHeight = nav.outerHeight();
+  "use strict";
+  var nav = $("nav");
+  var navHeight = nav.outerHeight();
 
-	$('.navbar-toggler').on('click', function () {
-		if (!$('#mainNav').hasClass('navbar-reduce')) {
-			$('#mainNav').addClass('navbar-reduce');
-		}
-	})
+  $(".navbar-toggler").on("click", function () {
+    if (!$("#mainNav").hasClass("navbar-reduce")) {
+      $("#mainNav").addClass("navbar-reduce");
+    }
+  });
 
-	// Preloader
-	$(window).on('load', function () {
-		if ($('#preloader').length) {
-			$('#preloader').delay(100).fadeOut('slow', function () {
-				$(this).remove();
-			});
-		}
-	});
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
 
-	// Back to top button
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 100) {
-			$('.back-to-top').fadeIn('slow');
-		} else {
-			$('.back-to-top').fadeOut('slow');
-		}
-	});
-	$('.back-to-top').click(function () {
-		$('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-		return false;
-	});
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-	/*--/ Star ScrollTop /--*/
-	$('.scrolltop-mf').on("click", function () {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 1000);
-	});
+  /*--/ Star ScrollTop /--*/
+  $(".scrolltop-mf").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 
-	/*--/ Star Counter /--*/
-	$('.counter').counterUp({
-		delay: 15,
-		time: 2000
-	});
+  /*--/ Star Counter /--*/
+  $(".counter").counterUp({
+    delay: 15,
+    time: 2000,
+  });
 
-	/*--/ Star Scrolling nav /--*/
-	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top - navHeight + 5)
-				}, 1000, "easeInOutExpo");
-				return false;
-			}
-		}
-	});
+  /*--/ Star Scrolling nav /--*/
+  $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - navHeight + 5,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
 
-	// Closes responsive menu when a scroll trigger link is clicked
-	$('.js-scroll').on("click", function () {
-		$('.navbar-collapse').collapse('hide');
-	});
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-	// Activate scrollspy to add active class to navbar items on scroll
-	$('body').scrollspy({
-		target: '#mainNav',
-		offset: navHeight
-	});
-	/*--/ End Scrolling nav /--*/
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: navHeight,
+  });
+  /*--/ End Scrolling nav /--*/
 
-	/*--/ Navbar Menu Reduce /--*/
-	$(window).trigger('scroll');
-	$(window).on('scroll', function () {
-		var pixels = 50;
-		var top = 1200;
-		if ($(window).scrollTop() > pixels) {
-			$('.navbar-expand-md').addClass('navbar-reduce');
-			$('.navbar-expand-md').removeClass('navbar-trans');
-		} else {
-			$('.navbar-expand-md').addClass('navbar-trans');
-			$('.navbar-expand-md').removeClass('navbar-reduce');
-		}
-		if ($(window).scrollTop() > top) {
-			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
-		} else {
-			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-		}
-	});
+  /*--/ Navbar Menu Reduce /--*/
+  $(window).trigger("scroll");
+  $(window).on("scroll", function () {
+    var pixels = 50;
+    var top = 1200;
+    if ($(window).scrollTop() > pixels) {
+      $(".navbar-expand-md").addClass("navbar-reduce");
+      $(".navbar-expand-md").removeClass("navbar-trans");
+    } else {
+      $(".navbar-expand-md").addClass("navbar-trans");
+      $(".navbar-expand-md").removeClass("navbar-reduce");
+    }
+    if ($(window).scrollTop() > top) {
+      $(".scrolltop-mf").fadeIn(1000, "easeInOutExpo");
+    } else {
+      $(".scrolltop-mf").fadeOut(1000, "easeInOutExpo");
+    }
+  });
 
-	/*--/ Star Typed /--*/
-	if ($('.text-slider').length == 1) {
-		var typed_strings = $('.text-slider-items').text();
-		var typed = new Typed('.text-slider', {
-			strings: typed_strings.split(','),
-			typeSpeed: 80,
-			loop: true,
-			backDelay: 1100,
-			backSpeed: 30
-		});
-	}
+  /*--/ Star Typed /--*/
+  if ($(".text-slider").length == 1) {
+    var typed_strings = $(".text-slider-items").text();
+    var typed = new Typed(".text-slider", {
+      strings: typed_strings.split(","),
+      typeSpeed: 80,
+      loop: true,
+      backDelay: 1100,
+      backSpeed: 30,
+    });
+  }
 
+  $.getJSON("../data.json", function (res) {
+    const portfolio = res.portfolio;
+    const ele = $(document).find("#work .projects");
+    ele.html("");
+    if (typeof portfolio == "object") {
+      portfolio.map((item) => {
+        ele.append(`<div class="col-md-4">
+					<div class="work-box">
+						<div class="work-img">
+							<a href="${item.image}" data-lightbox="gallery-mf">
+								<img src="${item.image}" alt="${item.name}"
+									class="img-fluid">
+							</a>
+						</div>
+						<div class="work-content">
+							<div class="row">
+								<div class="col-sm-8">
+									<h2 class="w-title">${item.name}</h2>
+									<div class="w-more">
+										<span class="w-ctegory">${item.category}</span> / <span class="w-date">${item.date}</span>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<a href="${item?.link}"
+										target="_blank" rel="nofollow noindex" class="w-like">
+										<span class="ion-ios-plus-outline"></span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>`);
+      });
+    }
+  });
 })(jQuery);
